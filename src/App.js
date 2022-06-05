b1f5ce01e0851dac866434673f34a4fe72e3d9f0
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-05 07:27:18 +0530

    Lesson-4 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog!";
+  const likes = 50;
+  // const person = { name: 'Ikshul', age: 20 };
+  const link = "https://www.google.com";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times </p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Hello, ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>GOOGLE SITE</a>
+      </div>
     </div>
   );
 }
