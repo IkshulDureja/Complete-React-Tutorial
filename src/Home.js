fc5286ea595d02a08775a94f53ef639cb0ea8976
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 08:28:32 +0530

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello, ninjas!", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("Hello " + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain("mario", e)}>Click me again</button>
         </div>
     );
 }
