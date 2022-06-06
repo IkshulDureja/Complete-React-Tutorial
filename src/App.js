e773ef490c27e3b6718d08f329b3d62ab3f9e038
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 20:34:00 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
@@ -8,11 +9,10 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route path="/">
-              <Home/>
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
         </div>
       </div>
     </Router>
