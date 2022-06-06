1e81b5aef93198e447a86b0d7ac1f9a0a2732b60
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 16:14:37 +0530

    UseEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,11 @@ const Home = () => {
         const newBlogs = blogs.filter((blog) => (blog.id !== id));
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log("Use Effect Ran")
+        console.log(blogs)
+    })
 
     return (
         <div className="home">
