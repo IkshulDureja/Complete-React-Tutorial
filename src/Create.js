9fe7d39b53e0df4eda6dbcc894a4e38cc3488295
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-07 00:39:14 +0530

    Programmatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const navigate= useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,6 +22,7 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setIsPending(false);
+            navigate("/");
         })
     }
 
