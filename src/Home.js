b5c46b74e59cdcb0ab9d484b7b2e28124d85e644
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 12:36:27 +0530

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,8 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog) => (blog.author === 'mario'))} title="Mario's Blogs" />
+
         </div>
     );
 }
