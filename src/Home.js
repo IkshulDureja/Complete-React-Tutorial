b07cd0e10608ccf9917d1d34bba73c9d98c07253
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 17:04:31 +0530

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,26 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
 
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+        }, 1000)
     }, []);
 
 
 
     return (
         <div className="home">
+            {isPending && <div>Loading..</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
