6419715a294dc6347156588cb19ca6f0ac642d70
Ikshul Dureja <ikshuldureja130@gmail.com>
2022-06-06 13:21:17 +0530

    Functions as Props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleClick }) => {
     // const blogs = props.blogs;
     // const title = props.title;
     // console.log(props, blogs)
@@ -9,6 +9,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview">
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleClick(blog.id)}>Delete Button</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,14 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleClick = (id) => {
+        const newBlogs = blogs.filter((blog) => (blog.id !== id));
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter((blog) => (blog.author === 'mario'))} title="Mario's Blogs" />
-
+            <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
         </div>
     );
 }
